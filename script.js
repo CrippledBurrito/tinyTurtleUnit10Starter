c15569955ef7ca476800dee0009e15d2d55ad97b
Bruno <bruno.h.student@lyonscommunityschool.org>
2017-03-29 14:26:16 +0000

    house

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
 function square(){
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
+    forward(50);
+    right(90);
+    forward(50);
+    right(90);
+    forward(50);
+    right(90);
+    forward(50);
+    right(90);
+    forward(50);
 }
 
+
+function triangle() {
+    forward(50);
+    right(60);
+    
+    forward(50);
+    right(60);
+    
+    forward(50);
+    right(60);
+}
+
+function rectangle() {
+    forward(0);
+    right(90);
+    
+    forward(87);
+    right(90);
+    
+}
 // Type your function call below
 
 
-stamp();
+
+
+triangle();
+
+square();
+
+rectangle();
+
+
